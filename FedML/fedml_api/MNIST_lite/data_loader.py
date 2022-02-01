39ef63e7903c69ca38d4b7596269a1ecee938beb
tedd-E <62875128+tedd-E@users.noreply.github.com>
2022-02-01 14:30:09 -0800

    updates to dataloader

--- FedML/fedml_api/MNIST_lite/data_loader.py
+++ FedML/fedml_api/MNIST_lite/data_loader.py
@@ -1,46 +1,36 @@
 import json
 import os
-
-import numpy as np
+import gzip
 import torch
+import struct
+import numpy as np
 
-
-# TODO: configure this to match new custom mobile preprocessor
+# TODO: configure to match mobilepreprocesor
 
 def read_data(train_data_dir, test_data_dir):
-    '''parses data in given train and test data directories
-
-    assumes:
-    - the data in the input directories are .json files with
-        keys 'users' and 'user_data'
-    - the set of train set users is the same as the set of test set users
-
-    Return:
-        clients: list of non-unique client ids
-        groups: list of group ids; empty list if none found
-        train_data: dictionary of train data
-        test_data: dictionary of test data
-    '''
-
     clients = []
     groups = []
     train_data = {}
     test_data = {}
 
-    train_files = os.listdir(train_data_dir)
-    train_files = [f for f in train_files if f.endswith('.json')]
-    for f in train_files:
-        file_path = os.path.join(train_data_dir, f)
-        with open(file_path, 'r') as inf:
-            cdata = json.load(inf)
-        clients.extend(cdata['users'])
-        if 'hierarchies' in cdata:
-            groups.extend(cdata['hierarchies'])
-        train_data.update(cdata['user_data'])
+    # train_files = os.listdir(train_data_dir)
+    # train_files = [f for f in train_files if f.endswith('.json')]
+    # for f in train_files:
+    #     file_path = os.path.join(train_data_dir, f)
+    #     with open(file_path, 'r') as inf:
+    #         cdata = json.load(inf)
+    #     clients.extend(cdata['users'])
+    #     if 'hierarchies' in cdata:
+    #         groups.extend(cdata['hierarchies'])
+    #     train_data.update(cdata['user_data'])
 
-    test_files = os.listdir(test_data_dir)
-    test_files = [f for f in test_files if f.endswith('.json')]
+    test_files = os.listdir()
+    test_files = [f for f in test_files if f.endswith('.gz')]
     for f in test_files:
+        magic, size = struct.unpack(">II", f.read(8))
+        nrows, ncols = struct.unpack(">II", f.read(8))
+        data = np.frombuffer(f.read(), dtype=np.dtype(np.uint8).newbyteorder('>'))
+        data = data.reshape((size, nrows, ncols))
         file_path = os.path.join(test_data_dir, f)
         with open(file_path, 'r') as inf:
             cdata = json.load(inf)
